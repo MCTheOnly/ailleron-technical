b7fe87338871e34b2d29b9b39ae662efdec6256f
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-09-08 06:09:07 +0200

    final

--- src/components/home/TwoColumns.jsx
+++ src/components/home/TwoColumns.jsx
@@ -11,7 +11,7 @@ const TwoColumns = () => {
 
 			<div className="two-columns__column two-columns__column--left">
 				<h2>Lorem ipsum dolor<br />Sit ament</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <strong>cillum dolore eu</strong> fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<ContactButton
 					name='Contact'
 					customClass={'contact-button'}
